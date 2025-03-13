d47196dd955bc3fe540984db1ba9788925e47bc2
Maxim Kuzmin <maxim.kuzmin@inbox.ru>
2025-03-13 17:32:08 +0300

    -

--- src/Backend/src/Reader/src/DomainUseCases/DummyItem/DummyItemExtensions.cs
+++ src/Backend/src/Reader/src/DomainUseCases/DummyItem/DummyItemExtensions.cs
@@ -25,4 +25,22 @@ public static class DummyItemExtensions
   {
     return new(items, totalCount);
   }
+
+  /// <summary>
+  /// Преобразовать к разделу сортировки в запросе фиктивных предметов.
+  /// </summary>
+  /// <param name="field">Поле сортировки.</param>
+  /// <param name="isDesc">Сортировать по убыванию?</param>
+  /// <returns>Pаздел сортировки в запросе.</returns>
+  public static QuerySortSection ToDummyItemQuerySortSection(this string? field, bool? isDesc)
+  {
+    field = (field ?? string.Empty).Trim();
+
+    if (field == string.Empty)
+    {
+      field = DummyItemSettings.DefaultQuerySortSection.Field;
+    }
+
+    return new(field, isDesc ?? DummyItemSettings.DefaultQuerySortSection.IsDesc);
+  }
 }
