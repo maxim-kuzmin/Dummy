5f1bfcda92a78a3418c911cfa873a971a81bbd89
Maxim Kuzmin <maxim.kuzmin@inbox.ru>
2025-04-11 02:26:23 +0300

    -

--- src/Backend/src/Writer/src/Apps/DbMigrationApp/App/AppService.cs
+++ src/Backend/src/Writer/src/Apps/DbMigrationApp/App/AppService.cs
@@ -3,9 +3,13 @@
 /// <summary>
 /// Сервис приложения.
 /// </summary>
+/// <param name="_hostApplicationLifetime">Жизненный цикл приложения.</param>
 /// <param name="_logger">Логгер.</param>
 /// <param name="_serviceScopeFactory">Фабрика области видимости сервисов.</param>
-public class AppService(ILogger<AppService> _logger, IServiceScopeFactory _serviceScopeFactory) : BackgroundService
+public class AppService(
+  IHostApplicationLifetime _hostApplicationLifetime,
+  ILogger<AppService> _logger,
+  IServiceScopeFactory _serviceScopeFactory) : BackgroundService
 {
   /// <inheritdoc/>
   protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -16,16 +20,18 @@ public class AppService(ILogger<AppService> _logger, IServiceScopeFactory _servi
 
       var appConfigOptionsSnapshot = scope.ServiceProvider.GetRequiredService<IOptionsSnapshot<AppConfigOptions>>();
 
-      var appConfigOptionsPostgreSQLSection = Guard.Against.Null(appConfigOptionsSnapshot.Value.PostgreSQL);
+      var db = appConfigOptionsSnapshot.Value.Db;
 
-      _logger.LogInformation("PostgreSQL: {appConfigOptionsPostgreSQLSection}", appConfigOptionsPostgreSQLSection);
+      _logger.LogInformation("Db: {db}", db);
 
       if (_logger.IsEnabled(LogLevel.Information))
       {
         _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
       }
 
-      await Task.Delay(Timeout.Infinite, stoppingToken);
+      await Task.Delay(0, stoppingToken);//await Task.Delay(Timeout.Infinite, stoppingToken);
+
+      _hostApplicationLifetime.StopApplication();
     }
   }
 }
