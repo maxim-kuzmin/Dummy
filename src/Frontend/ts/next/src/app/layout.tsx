99e418ea149463e38c8035dc866c5484f6f33c47
Maxim Kuzmin <maxim.kuzmin@inbox.ru>
2025-07-26 23:39:56 +0300

    -

--- src/Frontend/ts/next/src/app/layout.tsx
+++ src/Frontend/ts/next/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+import '../assets/styles/index.css'
 import AppLayout from '@/components/app/layout'
 
 const geistSans = Geist({
